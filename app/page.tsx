6a56368aa88b89b16716eb27a5a15906543df2e1
Impulse <impulses4mini@gmail.com>
2024-05-25 13:06:24 +0300

    complete setup prettier cfg

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from '@/components/ui/button';
-import { Camera } from 'lucide-react';
+import { Button } from "@/components/ui/button";
+import { Camera } from "lucide-react";
 const HomePage = () => {
   return (
     <div>
       <h1>Hello</h1>
-      <Button variant='outline'>click me</Button>
+      <Button variant="outline">click me</Button>
       <Camera />
     </div>
   );
