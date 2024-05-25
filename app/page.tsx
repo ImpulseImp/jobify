81b1d9f16eab63e6efab82bdc7ebaf662547a168
Impulse <impulses4mini@gmail.com>
2024-05-25 13:03:38 +0300

    test icon and btn

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
+import { Button } from '@/components/ui/button';
+import { Camera } from 'lucide-react';
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Button variant='outline'>click me</Button>
+      <Camera />
+    </div>
+  );
 };
 export default HomePage;
